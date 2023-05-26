0d9f699206c8d03282fc0151c70ef4ec42be56b3
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-05-26 18:37:27 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 import { StytchUIClient } from "https://www.unpkg.com/@stytch/vanilla-js@0.9.5/dist/index.esm.js";
-import dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { dotenv } from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
 // const STYTCH_PUBLIC_TOKEN = process.env.STYTCH_PUBLIC_TOKEN
